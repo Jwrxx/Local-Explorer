6a8575214397c093a6167fcf340b6a03f2f45ab6
johnwjr <jwalkjr115@gmail.com>
2025-04-29 12:27:08 -0400

    Added View Controller Classes

---
+++ Local Explorer/DiscoverViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiscoverViewController.swift
+//  Local Explorer
+//
+//  Created by John Walker Jr on 4/29/25.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
