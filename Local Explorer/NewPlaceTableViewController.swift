37385992e3ee2816272b62d7e8a2611794015719
johnwjr <jwalkjr115@gmail.com>
2025-04-30 09:38:08 -0400

    Implement data loading, table view population, and delete/place selection handling

--- Local Explorer/NewPlaceTableViewController.swift
+++ Local Explorer/NewPlaceTableViewController.swift
@@ -17,6 +17,8 @@ class NewPlaceTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDataFromDatabase()
+        tableView.reloadData()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -35,23 +37,38 @@ class NewPlaceTableViewController: UITableViewController {
             
         }
     }
+    
+    func loadDataFromDatabase() {
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Place")
+
+        do {
+            places = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return places.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath)
         cell.accessoryType = UITableViewCell.AccessoryType.detailDisclosureButton
+        
+        let place = places[indexPath.row] as? Place
+        cell.textLabel?.text = place?.placeName
+        //cell.detailTextLabel?.text = place?.locationDescription
 
         // Configure the cell...
 
@@ -67,17 +84,46 @@ class NewPlaceTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let place = places[indexPath.row] as! Place
+            let context = appDelegate.persistentContainer.viewContext
+            context.delete(place)
+            do {
+                try context.save()
+            } catch {
+                fatalError("Error saving context: \(error)")
+            }
+            loadDataFromDatabase()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let selectedPlace = places[indexPath.row] as! Place
+            let name = selectedPlace.placeName ?? "Unnamed"
+
+            let actionHandler: (UIAlertAction) -> Void = { _ in
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let controller = storyboard.instantiateViewController(withIdentifier: "PlaceController") as? NewPlaceViewController
+                controller?.currentPlace = selectedPlace
+                self.navigationController?.pushViewController(controller!, animated: true)
+            }
+
+            let alertController = UIAlertController(title: "Place Selected",
+                                                    message: "Selected row: \(indexPath.row) (\(name))", preferredStyle: .alert)
+            let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let actionDetails = UIAlertAction(title: "Show Details", style: .default, handler: actionHandler)
+
+            alertController.addAction(actionCancel)
+            alertController.addAction(actionDetails)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
     /*
     // Override to support rearranging the table view.
@@ -104,4 +150,4 @@ class NewPlaceTableViewController: UITableViewController {
     }
     */
 
-}
+
